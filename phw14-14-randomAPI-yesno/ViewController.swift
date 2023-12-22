1927780f73cf8e999c3b2aefb97b49e636b45478
jasonhung <hello@ifhavebugcallme.com>
2023-12-23 00:06:04 +0800

    Initial Commit

---
+++ phw14-14-randomAPI-yesno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  phw14-14-randomAPI-yesno
+//
+//  Created by jasonhung on 2023/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
